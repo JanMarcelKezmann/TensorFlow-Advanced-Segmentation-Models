18ac7b823ed4e9baf1f066a56b0a642474266254
Jan-Marcel Kezmann <50111329+JanMarcelKezmann@users.noreply.github.com>
2020-09-18 12:22:12 +0200

    Update pspnet.py
    Make backbones trainable

--- tf_segmentation_models/models/pspnet.py
+++ tf_segmentation_models/models/pspnet.py
@@ -5,22 +5,35 @@ from ._custom_layers_and_blocks import ConvolutionBnActivation, SpatialContextBl
 from ..backbones.tf_backbones import create_backbone
 
 class PSPNet(tf.keras.models.Model):
-    def __init__(self, n_classes, backbone, filters=256, activation="softmax",
-                 dropout=None, pooling_type="avg", backbone_output_layer_idx=0,
-                 final_upsample_factor=2, **kwargs):
+    def __init__(self, n_classes, base_model, output_layers, filters=256,
+                 final_activation="softmax", backbone_trainable=False,
+                 dropout=None, pooling_type="avg", final_upsample_factor=2, **kwargs):
         super(PSPNet, self).__init__()
         
         self.n_classes = n_classes
-        self.backbone = backbone
-        self.activation = activation
+        self.backbone = None
+        self.final_activation = final_activation
         self.filters = filters
         self.dropout = dropout
         self.pooling_type = pooling_type
-        self.backbone_output_layer_idx = backbone_output_layer_idx
         self.final_upsample_factor = final_upsample_factor
 
         axis = 3 if K.image_data_format() == "channels_last" else 1
 
+        if self.final_upsample_factor == 8:
+            output_layers = output_layers[:3]
+            self.final_upsample2d = tf.keras.layers.UpSampling2D(size=final_upsample_factor, interpolation="bilinear")
+        elif self.final_upsample_factor == 4:
+            output_layers = output_layers[:2]
+            self.final_upsample2d = tf.keras.layers.UpSampling2D(size=final_upsample_factor, interpolation="bilinear")
+        elif self.final_upsample_factor == 2:
+            output_layers = output_layers[:1]
+            self.final_upsample2d = tf.keras.layers.UpSampling2D(size=final_upsample_factor, interpolation="bilinear")
+        else:
+            raise ValueError("'final_upsample_factor' must be one of (2, 4, 8), got {}".format(self.final_upsample_factor))
+
+        base_model.trainable = backbone_trainable
+        self.backbone = tf.keras.Model(inputs=base_model.input, outputs=output_layers)
 
         # Define Layers
         self.spatial_context_block_1 = SpatialContextBlock(1, filters, pooling_type)
@@ -31,38 +44,36 @@ class PSPNet(tf.keras.models.Model):
         self.concat = tf.keras.layers.Concatenate(axis=axis)
         self.conv1x1_bn_relu = ConvolutionBnActivation(filters, (1, 1))
 
-        self.spatial_dropout = tf.keras.layers.SpatialDropout2D(dropout)
+        if dropout is not None:
+            self.spatial_dropout = tf.keras.layers.SpatialDropout2D(dropout)
 
         self.final_conv3x3 = tf.keras.layers.Conv2D(self.n_classes, (3, 3), strides=(1, 1), padding='same')
-        self.final_upsample2d = tf.keras.layers.UpSampling2D(size=final_upsample_factor, interpolation="bilinear")
-        self.final_activation = tf.keras.layers.Activation(activation)
+        self.final_activation = tf.keras.layers.Activation(final_activation)
     
     
     def call(self, inputs, training=None, mask=None):
         if training is None:
             training = True
 
-        x = self.backbone(inputs)[self.backbone_output_layer_idx]
+        if self.final_upsample_factor == 2:
+            x = self.backbone(inputs)
+        else:
+            x = self.backbone(inputs)[-1]
 
         if K.image_data_format() == "channels_last":
             if x.shape[1] % 6 != 0 or x.shape[2] % 6 != 0:
                 raise ValueError("Height and Width of the backbone output must be divisible by 6, i.e. \
-                input_height or input_width / (backbone_output_layer_idx * 2 + 2) must be divisble by 6.")
+                input_height or input_width / final_upsample_factor must be divisble by 6.")
         else:
             if x.shape[2] % 6 != 0 or x.shape[2] % 6 != 0:
                 raise ValueError("Height and Width of the backbone output must be divisible by 6, i.e. \
-                input_height or input_width / (backbone_output_layer_idx * 2 + 2) must be divisble by 6.")
+                input_height or input_width / final_upsample_factor must be divisble by 6.")
 
 
         x1 = self.spatial_context_block_1(x, training=training)
         x2 = self.spatial_context_block_2(x, training=training)
         x3 = self.spatial_context_block_3(x, training=training)
         x6 = self.spatial_context_block_4(x, training=training)
-
-        # print("Shape x1: " + str(x1.shape))
-        # print("Shape x2: " + str(x2.shape))
-        # print("Shape x3: " + str(x3.shape))
-        # print("Shape x6: " + str(x6.shape))
 
         x = self.concat([x1, x2, x3, x6])
         x = self.conv1x1_bn_relu(x, training=training)
