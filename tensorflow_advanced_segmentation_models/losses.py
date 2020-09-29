f0d7eb88c7e54130f0691db80d5c066a08682786
JanMarcelKezmann <jankezmann@t-online.de>
2020-09-29 10:09:13 +0200

    Added New losses: Tversky and Combo Loss

--- tensorflow_advanced_segmentation_models/losses.py
+++ tensorflow_advanced_segmentation_models/losses.py
@@ -37,6 +37,46 @@ class DiceLoss(Loss):
             threshold=None
         )
 
+
+class TverskyLoss(Loss):
+    def __init__(self, alpha=0.7, class_weights=None, smooth=1e-5):
+        super().__init__(name="tversky_loss")
+        self.alpha = alpha
+        self.class_weights = class_weights if class_weights is not None else 1.0
+        self.smooth = smooth
+
+    def __call__(self, y_true, y_pred):
+        return 1.0 - tversky(
+            y_true,
+            y_pred,
+            alpha=self.alpha,
+            class_weights=self.class_weights,
+            smooth=self.smooth,
+            threshold=None
+        )
+
+
+class FocalTverskyLoss(Loss):
+    def __init__(self, alpha=0.7, gamma=1.25, class_weights=None, smooth=1e-5):
+        super().__init__(name="focal_tversky_loss")
+        self.alpha = alpha
+        self.gamma = gamma
+        self.class_weights = class_weights if class_weights is not None else 1.0
+        self.smooth = smooth
+
+    def __call__(self, y_true, y_pred):
+        return K.pow((1.0 - tversky(
+            y_true,
+            y_pred,
+            alpha=self.alpha,
+            class_weights=self.class_weights,
+            smooth=self.smooth,
+            threshold=None
+        )),
+        self.gamma
+        )
+
+
 class BinaryCELoss(Loss):
     def __init__(self):
         super().__init__(name="binary_crossentropy")
@@ -82,3 +122,24 @@ class BinaryFocalLoss(Loss):
 
     def __call__(self, y_true, y_pred):
         return binary_focal_loss(y_true, y_pred, alpha=self.alpha, gamma=self.gamma)
+
+        class ComboLoss(Loss):
+    def __init__(self, alpha=0.5, beta=1.0, ce_ratio=0.5, class_weights=None, smooth=1e-5):
+        super().__init__(name="combo_loss")
+        self.alpha = alpha
+        self.beta = beta
+        self.ce_ratio = ce_ratio
+        self.class_weights = class_weights if class_weights is not None else 1.0
+        self.smooth = smooth
+
+    def __call__(self, y_true, y_pred):
+        return combo(
+            y_true,
+            y_pred,
+            alpha=self.alpha,
+            beta=self.beta,
+            ce_ratio=self.ce_ratio,
+            class_weights=self.class_weights,
+            smooth=self.smooth,
+            threshold=None
+        )
